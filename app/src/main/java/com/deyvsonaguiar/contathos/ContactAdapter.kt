ceee688a4448d8b718f128b3e9978ed590388c87
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-03-29 14:04:33 -0400

    Criação do recyclerView na main_activity.xml

--- app/src/main/java/com/deyvsonaguiar/contathos/ContactAdapter.kt
+++ app/src/main/java/com/deyvsonaguiar/contathos/ContactAdapter.kt
@@ -3,6 +3,7 @@ package com.deyvsonaguiar.contathos
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -10,13 +11,26 @@ class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHold
     private val lista: MutableList<Contact> = mutableListOf()
 
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(contact: Contact) {
 
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivImage: TextView = itemView.findViewById(R.id.iv_photograph)
+
+        fun bind(contact: Contact) {
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
         }
     }
 
+    fun UpdateList(list: List<Contact>) {
+        this.lista.clear()
+        this.lista.addAll(list)
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate()
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
