58eebf9048a86b3e4187b5c121b48d1e6e1f061d
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-03-29 13:33:05 -0400

    Criação de contact_item.xml

---
+++ app/src/main/java/com/deyvsonaguiar/contathos/Contact.kt
@@ -0,0 +1,7 @@
+package com.deyvsonaguiar.contathos
+
+class Contact {
+    var name: String,
+    var phone: String,
+    var photograph: String
+}
--- app/src/main/java/com/deyvsonaguiar/contathos/ContactAdapter.kt
+++ app/src/main/java/com/deyvsonaguiar/contathos/ContactAdapter.kt
@@ -1,24 +1,30 @@
 package com.deyvsonaguiar.contathos
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
 
+    private val lista: MutableList<Contact> = mutableListOf()
+
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(contact: Contact) {
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate()
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(lista[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return lista.size
     }
 
 
