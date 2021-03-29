1e978f3e64206e8a1d4c5c6c27a072bad8d80327
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-03-29 12:14:02 -0400

    Iniciando projeto com ContactAdapter

---
+++ app/src/main/java/com/deyvsonaguiar/contathos/ContactAdapter.kt
@@ -0,0 +1,25 @@
+package com.deyvsonaguiar.contathos
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
+
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
